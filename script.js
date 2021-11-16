836417b0036175969361e3bd97712cce8fa6afda
quedacoder <quedacoder@gmail.com>
2021-11-15 22:18:46 -0600

    close #3 add game logic

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
 'use strict';
 
-/*document.querySelector('.message').textContent = '🥳 Correct Number!';
+// generates random number between 1 - 20
+var secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// set score and highscore
+var score = 20,
+    highscore = 0;
+document.querySelector('.score').textContent = 20;
+document.querySelector('.highscore').textContent = highscore;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);*/
-
-// using the addEventListener to trigger click event on button
+// EventListener for click of button check
 document.querySelector('.check').addEventListener('click', function(e) {
     const guess = Number(document.querySelector('.guess').value);
 
     console.log(guess, typeof guess);
 
-    if (!guess) {
-        document.querySelector('.message').textContent = '⛔️ No number!!!';
+    if (score > 0) {
+        if (!guess) {
+            document.querySelector('.message').textContent = '⛔️ No number!!!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else if (guess === secretNumber) {
+            document.querySelector('.message').textContent =
+                '🥳 Winner Winner!!! Chicken Dinner!!!';
+            document.querySelector('.number').textContent = secretNumber;
+            // set highscore
+            if (score > highscore) {
+                highscore = score;
+                document.querySelector('.highscore').textContent = score;
+            }
+        } else if (guess > secretNumber) {
+            document.querySelector('.message').textContent = '📉  Too High!!!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else if (guess < secretNumber) {
+            document.querySelector('.message').textContent = '📈  Too Low!!!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+    } else {
+        document.querySelector('.message').textContent = 'You lost the game!!!!';
     }
+});
+
+// Event Listner for click of button again
+document.querySelector('.again').addEventListener('click', function() {
+    // reset input, score, message, generate new random number
+    document.querySelector('.score').textContent = 20;
+    score = 20;
+    document.querySelector('.guess').value = 0;
+    document.querySelector('.message').textContent = 'Start Guessing.....';
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.number').textContent = '?';
 });
